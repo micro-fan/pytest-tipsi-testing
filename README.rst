7062e2745f6638cb0bae144aed736a23175e07b9
Kirill Pinchuk <cybergrind@gmail.com>
2017-09-27 21:38:20 +0300

    Initial commit

---
+++ README.rst
@@ -0,0 +1,24 @@
+pytest-tipsi-testing
+====================
+
+-----
+
+.. contents:: **Table of Contents**
+    :backlinks: none
+
+Installation
+------------
+
+pytest-tipsi-testing is distributed on `PyPI <https://pypi.org>`_ as a universal
+wheel and is available on Linux/macOS and Windows and supports
+Python 3.5+.
+
+.. code-block:: bash
+
+    $ pip install pytest-tipsi-testing
+
+License
+-------
+
+pytest-tipsi-testing is distributed under the terms of the
+`MIT License <https://choosealicense.com/licenses/mit>`_.
---
+++ setup.py
@@ -0,0 +1,50 @@
+from io import open
+
+from setuptools import find_packages, setup
+
+with open('pytest_tipsi_testing/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+REQUIRES = []
+
+setup(
+    name='pytest-tipsi-testing',
+    version=version,
+    description='',
+    long_description=readme,
+    author='cybergrind',
+    author_email='cybergrind@gmail.com',
+    maintainer='cybergrind',
+    maintainer_email='cybergrind@gmail.com',
+    url='https://github.com/tipsi/pytest-tipsi-testing',
+    license='MIT',
+
+    keywords=[
+        'pytest', 'helpers', 'fixtures',
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+
+    install_requires=REQUIRES,
+    tests_require=['coverage', 'pytest'],
+
+    packages=find_packages(),
+)
